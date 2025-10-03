90fe5820e99330ecfe6dfe3711eec939f4be1e66
machariadenis036-coder <machariadenis036@gmail.com>
2025-10-03 15:11:22 +0300

    Add files via upload

---
+++ Units.c
@@ -0,0 +1,19 @@
+//simple c program 
+/* 
+Name:Denis macharia nguru 
+Reg:CT100/G/26250/25 
+Description:units 
+*/ 
+#include<stdio.h> 
+int main() { 
+printf("SEMESTER UNITS");
+printf("\n Essential of accounting");
+printf("\n Analogue electronics"); 
+printf("\n Computer system and organisation"); 
+printf("\n Mathematics for science"); 
+printf("\n Fundamental of ICT"); 
+printf("\n Communication skills"); 
+printf("\n Introduction to programming and algorithms"); 
+printf("\n Introduction to computer applications"); 
+return 0; 
+}
