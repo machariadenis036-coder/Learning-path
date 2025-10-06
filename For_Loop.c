fc243533d49994c4ed2aa9a9d8e0c90287817fe4
machariadenis036-coder <machariadenis036@gmail.com>
2025-10-06 13:05:28 +0300

    Add files via upload

---
+++ For_Loop.c
@@ -0,0 +1,17 @@
+/*
+Name:Denis Macharia Nguru
+Reg:CT100/G/26250/25
+For Loop
+*/
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    // Loop from 100 down to 50
+    for (i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
