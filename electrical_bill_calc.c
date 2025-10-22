1dfe68a7fd2506b88bfb04b111c792bd5f0d7caa
machariadenis036-coder <machariadenis036@gmail.com>
2025-10-22 17:23:17 +0300

    Add files via upload

--- electrical_bill_calc.c
+++ electrical_bill_calc.c
@@ -1,25 +1,32 @@
 /*
 Name:Denis Macharia Nguru
-Reg No:CT100/G/26250/25
-Distance calculations
+REG No:CT100/G/26250/25
+Electrical calculations
 */
+
 #include <stdio.h>
 
-// Function to calculate fare
-int calculateFare(int distance) {
-    int rate = 50; // Fare rate per kilometer
-    int totalFare = distance * rate;
-    return totalFare;
+float calculateElectricBill(int units) {
+    float bill;
+
+    if (units <= 100) {
+        bill = units * 10;
+    } 
+    else if (units <= 200) {
+        bill = (100 * 10) + ((units - 100) * 15);
+    } 
+    else {
+        bill = (100 * 10) + (100 * 15) + ((units - 200) * 20);
+    }
+
+    return bill;
 }
 
-// Example usage
 int main() {
-    int distance;
-    printf("Enter distance traveled (in km): ");
-    scanf("%d", &distance);
+    int units;
+    printf("Enter number of units consumed: ");
+    scanf("%d", &units);
 
-    int fare = calculateFare(distance);
-    printf("Total fare: KSh. %d\n", fare);
-
+    printf("Total bill = KSh. %.2f\n", calculateElectricBill(units));
     return 0;
 }
